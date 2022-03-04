c5a67d78a878dd1b85287fa145e1f4bf1225907d
root <leoliu00529@gmail.com>
2022-03-04 00:30:30 +0000

    Client and server backbone. Client can talk to server via UDP.

--- client.cpp
+++ client.cpp
@@ -1,8 +1,70 @@
-#include <string>
-#include <thread>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <csignal>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 
-int main()
-{
-  std::cerr << "client is not implemented yet" << std::endl;
+int main(int argc, const char * argv[]) {
+    
+    int portNumber, sock;
+    struct sockaddr_in socketAddress;
+    int bytes_sent;
+    char buffer[200];
+    
+    // Validate cml arguments.
+    
+    if (argc != 4) {
+        std::cerr << "ERROR: Invalid number of arguments. Need IP address, port number, and destination directory.";
+        
+        exit(1);
+    }
+    
+    try {
+        portNumber = std::stoi(argv[2]);
+        if (portNumber <= 0) {
+            std::cerr << "ERROR: Non-positive port number.";
+            exit(1);
+        }
+    } catch (std::exception const &e) {
+        std::cerr << "ERROR: Port number specified cannot be parsed.";
+        exit(1);
+    }
+    
+    //todo:: validate hostname or IP address.
+    
+    
+    
+    // Initialize socket, send dummy data to server.
+    
+    strcpy(buffer, "hello world!");
+     
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == -1) {
+        std::cerr << "Failed to initilaize socket.";
+        exit(1);
+    }
+
+    memset(&socketAddress, 0, sizeof socketAddress);
+
+    socketAddress.sin_family = AF_INET;
+    socketAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+    socketAddress.sin_port = htons(portNumber);
+
+    bytes_sent = sendto(sock, buffer, strlen(buffer), 0,(struct sockaddr*)&socketAddress, sizeof socketAddress);
+    if (bytes_sent < 0) {
+        std::cerr << "Sending to server failed.";
+        exit(1);
+    }
+
+    close(sock);
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,83 @@
-#include <string>
-#include <thread>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <csignal>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
 
-int main()
-{
-  std::cerr << "server is not implemented yet" << std::endl;
+void signalHandler(int sig) {
+    // todo: clean up, graceful exit.
+    
+    
+    // exit with code zero, as specified by the spec.
+    exit(0);
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int portNumber, sock;
+    struct sockaddr_in socketAddress;
+    char buffer[1024];
+    ssize_t recsize;
+    socklen_t fromlen;
+    
+    // Validate cml arguments. Port number needs to be postive integer.
+    // Destination directory is guaranteed to be correct.
+    
+    if (argc != 3) {
+        std::cerr << "ERROR: Invalid number of arguments. Need port number and destination directory.";
+        exit(1);
+    }
+    
+    try {
+        portNumber = std::stoi(argv[1]);
+        if (portNumber <= 0) {
+            std::cerr << "ERROR: Non-positive port number.";
+            exit(1);
+        }
+    } catch (std::exception const &e) {
+        std::cerr << "ERROR: Port number specified cannot be parsed.";
+        exit(1);
+    }
+    
+    // Register signal handler to handle SIGQUIT and SIGTERM
+    
+    signal(SIGQUIT, signalHandler);
+    signal(SIGTERM, signalHandler);
+    
+    // Initial like socket, bind, and receive.
+    
+    memset(&socketAddress, 0, sizeof socketAddress);
+    socketAddress.sin_family = AF_INET;
+    socketAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    socketAddress.sin_port = htons(portNumber);
+    fromlen = sizeof socketAddress;
+
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (bind(sock, (struct sockaddr *)&socketAddress, sizeof socketAddress) == -1) {
+        std::cerr << "ERROR: Failed to bind socket.";
+        close(sock);
+        exit(1);
+    }
+
+    for (;;) {
+        recsize = recvfrom(sock, (void*)buffer, sizeof buffer, 0, (struct sockaddr*)&socketAddress, &fromlen);
+        if (recsize < 0) {
+            std::cerr << "Negative receive size.";
+            exit(1);
+        }
+        printf("recsize: %d\n ", (int)recsize);
+        sleep(1);
+        printf("datagram: %.*s\n", (int)recsize, buffer);
+    }
+    
+    return 0;
 }
