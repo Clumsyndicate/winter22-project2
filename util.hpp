36974c7cea89a3855397cb6d1166bb03d308e54e
root <leoliu00529@gmail.com>
2022-03-08 18:36:21 +0000

    Change buf2int. Now it should work.

--- util.hpp
+++ util.hpp
@@ -14,14 +14,20 @@ const int INIT_SS_THRESH = 10000; // bytes
 
 uint32_t buf2int(const char *s, size_t a, size_t b) {
     int val = 0;
+
     while (a < b) {
-       val = val * 256 + s[a++];
+        const unsigned char cur = s[a];
+        unsigned int curInt =  cur;
+        
+        val = val * 256 + curInt;
+        a ++;
     }
+
     return val;
 }
 
 void int2buf(char *buf, uint32_t num, size_t a, size_t b) {
-    for (int i=b-1; i>=(int) a; i--) {
+    for (long i=b-1; i>=(int) a; i--) {
         buf[i] = num % 256;
         num /= 256;
     }
